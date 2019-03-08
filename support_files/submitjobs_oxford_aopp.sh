e971d194fe87c7e9e7348a6c854e5d9d315c89cb
Tim Lichtenberg <lichtenberg.tim@gmail.com>
2019-03-08 17:08:20 +0000

    Started w/ job script for Oxford cluster

---
+++ support_files/submitjobs_oxford_aopp.sh
@@ -0,0 +1,40 @@
+# Run prefix
+prefix=${PWD##*/}
+
+echo "#!/bin/csh -f" >> submit.sh
+
+# SLURM commands (man sbatch / http://www.arc.ox.ac.uk/content/slurm-job-scheduler)
+echo "#SBATCH --job-name=$prefix" >> submit.sh
+echo "#SBATCH --output=$prefix.out" >> submit.sh
+echo "#SBATCH --error=$prefix.err" >> submit.sh
+echo "#SBATCH --time=100:00:00" >> submit.sh
+echo "#SBATCH --ntasks=1" >> submit.sh
+echo "#SBATCH --mem=3000" >> submit.sh
+echo "#SBATCH -p priority-rp" >> submit.sh
+echo "#SBATCH --cpus-per-task=1" >> submit.sh
+
+# Print date and time before job runs
+echo "date" >> submit.sh
+
+# Print current working directory
+echo "Working directory: $cwd" >> submit.sh
+
+# Load environment
+echo Working directory: $cwd
+echo "module load netcdf/4.4.1__intel2015" >> submit.sh
+echo "module load intel-compilers/2015" >> submit.sh
+echo "module load intel-mpi/2015" >> submit.sh
+echo "module load intel-mkl/2015" >> submit.sh
+
+# Generate initial conditions and run the code
+echo "./in2mart" >> submit.sh
+echo "./i2mart" >> submit.sh
+
+#Print date and time after job finished
+echo "date" >> submit.sh
+
+# Submit created script
+sbatch ./submit.sh
+
+# Delete submit file after jobs are submitted
+rm -r submit.sh
